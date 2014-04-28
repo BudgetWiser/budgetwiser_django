7c323ed9cb9df8e6de40eb94681d4d26e1faa20f
Jong-Hyuk, Jung <heretor92@gmail.com>
2014-04-29 06:24:46 +0900

    Made annote app. And set urls.py for annote/urls.py

--- budgetwiser/budgetwiser/urls.py
+++ budgetwiser/budgetwiser/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'budgetwiser.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^annote/', include('budgetwiser.apps.annote.urls')),
 
+    # Media path
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.MEDIA_ROOT}),
+
+    # Admin page
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# Examples:
+# url(r'^$', 'budgetwiser.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
